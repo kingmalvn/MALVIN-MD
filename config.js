e89b4c66de68843cfbc26af4e26fb0e3337669d4
⚜️ ╰‿╯ MR Pabasara Malinda  ད★࿐ <142970254+CyberPabuZa0@users.noreply.github.com>
2024-11-23 18:20:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "EC1l1RKZ#iIk20jIp2r7-3tmLs0h2qfqTCnPfenAgsis_CVXc-3w",
+SESSION_ID: process.env.SESSION_ID || "xSFSkb4b#fdkdylSLDJsPfXvRfv9Kqi9BWXN9Us8N0tgmc7axflE",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/n5vvij.jpg",
 SUDO_NB: process.env.SUDO_NB || "263780934873",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
