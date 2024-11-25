f497b865e161c5c6dcd7f9047bea5cb7bae3a8d2
⚜️ ╰‿╯ MR Pabasara Malinda  ད★࿐ <142970254+CyberPabuZa0@users.noreply.github.com>
2024-11-25 19:03:58 +0530

    news.js

--- plugins/news.js
+++ plugins/news.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, reply }) => {
 ⚠️ _${article.description}_
 🔗 _${article.url}_
 
-  ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍᴀʟᴠɪɴ ᴋɪɴɢ
+  ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ Qᴜᴇᴇɴ ɴᴇᴛʜᴜ-ᴍᴅ
             `;
 
             console.log('Article URL:', article.urlToImage); // Log image URL for debugging
